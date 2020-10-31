a1fb93061fa102543a981494a82bf28fea9ae16e
skupchenkoalex <skupchenkoalex@gmail.com>
2020-10-31 22:34:44 +0300

    Initial commit

---
+++ client/src/main/java/client/less4.java
@@ -0,0 +1,76 @@
+package client;
+
+public class less4 {
+
+    private final Object mon = new Object();
+    private volatile char currentLetter = 'A';
+
+    public static void main(String[] args) {
+        less4 w = new less4();
+        Thread t1 = new Thread(() -> {
+            w.printA();
+        });
+        Thread t2 = new Thread(() -> {
+            w.printB();
+        });
+        Thread t3 = new Thread(() -> {
+            w.printC();
+        });
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+
+    public void printA() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'A') {
+                        mon.wait(10);
+                    }
+                    System.out.print("A");
+                    currentLetter = 'B';
+                    mon.notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printB() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'B') {
+                        mon.wait(10);
+                    }
+                    System.out.print("B");
+                    currentLetter = 'C';
+                    mon.notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printC() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'C') {
+                        mon.wait(10);
+                    }
+                    System.out.print("C");
+                    currentLetter = 'A';
+                    mon.notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
